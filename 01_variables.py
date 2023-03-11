f73e60e3ff93d32c5f28dd8f363d7b5314ef8b30
kosovito <kosovito@gmail.com>
2023-03-11 12:19:36 +0100

    Iniciamos

---
+++ 01_variables.py
@@ -0,0 +1,17 @@
+# Variables
+
+my_variable = "My String variable"
+print(my_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_bool_variable = False
+print(my_bool_variable)
+
+# print admite todo tipo de variables, y concatenarlas indistintamente
+print(my_variable, my_int_variable, my_bool_variable)
+
+# casteo de variables
+my_int_to_str = str(my_int_variable)
+print(type(my_int_to_str))
