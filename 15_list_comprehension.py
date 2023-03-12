9bd8b1afcb4f1894abf19a99401e0f32513c6d08
kosovito <kosovito@gmail.com>
2023-03-12 20:23:19 +0100

    Seguimos con nivel intermedio

---
+++ 15_list_comprehension.py
@@ -0,0 +1,37 @@
+# Clase en vídeo: https://youtu.be/TbcEqkabAWU?t=3239
+
+### List Comprehension ###
+
+my_original_list = [0, 1, 2, 3, 4, 5, 6, 7]
+print(my_original_list)
+
+my_range = range(8)
+print(list(my_range))
+
+# Definición
+
+my_list = [i + 1 for i in range(8)]
+print(my_list)
+
+my_list = [i * 2 for i in range(8)]
+print(my_list)
+
+my_list = [i * i for i in range(8)]
+print(my_list)
+
+
+def sum_five(number):
+    return number + 5
+
+
+my_list = [sum_five(i) for i in range(8)]
+print(my_list)
+
+# combinando con if
+odd_numbers = [i for i in range(21) if i % 2 != 0]  # numeros impares range 0 to 21
+print(odd_numbers) 
+
+# aplanando una lista de listas
+list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+flattened_list = [ number for row in list_of_lists for number in row]
+print(flattened_list)    # [1, 2, 3, 4, 5, 6, 7, 8, 9]
